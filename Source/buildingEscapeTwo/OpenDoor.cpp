0cf20abd4b5aa4aa6a1096282c703233a1ae13de
Wikonator <topes.jebal@gmail.com>
2019-04-20 23:29:32 +0200

    added the position reporter and Open door classes

---
+++ Source/buildingEscapeTwo/OpenDoor.cpp
@@ -0,0 +1,35 @@
+// Magos Dominus Faustinius 2019
+
+
+#include "OpenDoor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	FRotator Rotation = GetOwner()->GetActorRotation();
+
+	UE_LOG(LogTemp, Warning, TEXT("The rotation is %s"), *Rotation);
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
